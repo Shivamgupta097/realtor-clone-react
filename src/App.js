61c755804ba630acc58fb2af7d5b3b1aec9965d2
Shivamgupta097 <shivam.applicant@gmail.com>
2022-10-02 11:50:07 +0530

    Added route nad pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import {BrowserRouter as Router , Routes ,Route} from "react-router-dom";
+import {Home, Profile, SignIn, SignUp, ForgotPassword , Offers} from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      Hello workd
-    </div>
+    <Router>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-n" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/offers" element={<Offers/>}/>
+
+
+      </Routes>
+    </Router>
   );
 }
 
